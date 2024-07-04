cc0da5bcb4db708ff5d1023c5c7494275a3b1be3
XiaoWu <1610365132@qq.com>
2024-07-04 16:16:13 +0800

    Gitignore没调好，有些重要代码文件没加上。

---
+++ Page Navigation App/Windows/ProcessWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using iNKORE.UI.WPF.Modern.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Page_Navigation_App.Windows
+{
+    /// <summary>
+    /// ProcessBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProcessWindow : Window
+    {
+        public ProcessWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetProgress(double value)
+        {
+            Progress.Value = value;
+        }
+
+        public void SetStatus(string status)
+        {
+            Label.Content = status;
+        }
+    }
+}
